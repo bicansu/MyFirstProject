f9527c6e2429fe0c0716f501c1fcc5605b05a8d3
suruc <suruc@RAMAZANSURUCU>
2022-10-23 17:23:18 +0300

    Add project files.

---
+++ MyFirstProject/Controllers/AdminUrunlerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+using MyFirstProject.Models.Entities;
+
+
+namespace MyFirstProject.Controllers
+{
+    public class AdminUrunlerController : Controller
+    {
+        DbShopperProjectEntities db = new DbShopperProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblProducts.ToList();
+            return View(values);
+        }
+        public ActionResult DeleteProduct(int id)
+        {
+            var values = db.TblProducts.Find(id);
+            db.TblProducts.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(TblProducts p)
+        {
+            db.TblProducts.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            var values = db.TblProducts.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult UpdateProduct(TblProducts p)
+        {
+            var values = db.TblProducts.Find(p.ProductID);
+            values.ProductName = p.ProductName;
+            values.Description = p.Description;
+            values.Price = p.Price;
+            values.Stock = p.Stock;
+            values.CategoryID = p.CategoryID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
---
+++ MyFirstProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstProject.MvcApplication" Language="C#" %>
